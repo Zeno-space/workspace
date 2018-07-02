c5e819c031dbe103ed432de3a985eba88fecace5
Zeno <hsl40@qq.com>
2018-07-02 13:32:51 +0800

    .

---
+++ courses_system/core/pickle_db.py
@@ -0,0 +1,24 @@
+class Teacher:
+    pass
+
+class Student:
+    pass
+
+    
+class School:
+    pass
+
+class Class:
+    pass
+
+class Course:
+    pass
+
+
+
+bj_school = School('beijing')
+shh_school = School('shanghai')
+
+linux = Course('linux')
+python = Course('python')
+go = Course('go')
